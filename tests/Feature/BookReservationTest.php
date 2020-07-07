eebcf869d78f732a10979704f235bc2f98139aa9
Mahmoud Ebada <>
2020-07-07 14:46:07 +0200

    test
    a_book_can_be_added_to_libary

---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Book;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BookReservationTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function a_book_can_be_added_to_libary()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/books', [
+            'title' => 'Laravel TDD',
+            'author' => 'Ian Schinder'
+        ]);
+        
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+    }
+}
